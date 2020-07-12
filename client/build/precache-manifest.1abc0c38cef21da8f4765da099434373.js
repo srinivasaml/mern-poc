8e6fdb28bb03a3c83ab9a39ef1fc398ad235183c
Srinivasa L <srinil16@in.ibm.com>
2020-07-12 19:17:05 +0530

    client changes

---
+++ client/build/precache-manifest.1abc0c38cef21da8f4765da099434373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef088c1435545fc06fe625b6e32926e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33463d9d2a3917c71a1a",
+    "url": "/static/css/main.0393aba7.chunk.css"
+  },
+  {
+    "revision": "4382e480c680a516bc62",
+    "url": "/static/js/2.57552606.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.57552606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33463d9d2a3917c71a1a",
+    "url": "/static/js/main.fbf9e4c5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
